a314980e5852ab1252c104e3862d0f8671865bf2
Kometha <kenethcubas2002@unitec.edu>
2024-02-17 00:45:06 -0600

    primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'proy_ing_soft2';
+  constructor(private messageService: MessageService) { }
+
+  showSuccess() {
+    this.messageService.add({severity:'success', summary: 'Success', detail: 'Data Saved'});
+  }
+
 }
+
---
+++ src/app/modules/prime-ng/prime-ng.module.ts
@@ -0,0 +1,91 @@
+import { NgModule } from '@angular/core';
+
+import { ButtonModule } from 'primeng/button';
+import { DropdownModule } from 'primeng/dropdown';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AutoFocusModule } from 'primeng/autofocus';
+import { InputTextModule } from 'primeng/inputtext';
+import { ProgressBarModule } from 'primeng/progressbar';
+import { ProgressSpinnerModule } from 'primeng/progressspinner';
+import { TableModule } from 'primeng/table';
+import { ToastModule } from 'primeng/toast';
+import { AccordionModule } from 'primeng/accordion';
+import { AutoCompleteModule } from 'primeng/autocomplete';
+import { AvatarModule } from 'primeng/avatar';
+import { AvatarGroupModule } from 'primeng/avatargroup';
+import { BadgeModule } from 'primeng/badge';
+import { BreadcrumbModule } from 'primeng/breadcrumb';
+import { CalendarModule } from 'primeng/calendar';
+import { CardModule } from 'primeng/card';
+import { CarouselModule } from 'primeng/carousel';
+import { CascadeSelectModule } from 'primeng/cascadeselect';
+import { CheckboxModule } from 'primeng/checkbox';
+import { ChipModule } from 'primeng/chip';
+import { ChipsModule } from 'primeng/chips';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import { ConfirmPopupModule } from 'primeng/confirmpopup';
+import { ColorPickerModule } from 'primeng/colorpicker';
+import { ContextMenuModule } from 'primeng/contextmenu';
+import { DataViewModule } from 'primeng/dataview';
+import { DialogModule } from 'primeng/dialog';
+import { DividerModule } from 'primeng/divider';
+import { FieldsetModule } from 'primeng/fieldset';
+import { FileUploadModule } from 'primeng/fileupload';
+import { TabMenuModule } from 'primeng/tabmenu';
+import { TabViewModule } from 'primeng/tabview';
+import { TagModule } from 'primeng/tag';
+import { TerminalModule } from 'primeng/terminal';
+import { TieredMenuModule } from 'primeng/tieredmenu';
+import { TimelineModule } from 'primeng/timeline';
+import { ToggleButtonModule } from 'primeng/togglebutton';
+import { ToolbarModule } from 'primeng/toolbar';
+import { TooltipModule } from 'primeng/tooltip';
+
+
+@NgModule({
+  exports: [
+    ButtonModule,
+    DropdownModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    AutoFocusModule,
+    InputTextModule,
+    ProgressBarModule,
+    ProgressSpinnerModule,
+    TableModule,
+    ToastModule,
+    AccordionModule,
+    AutoCompleteModule,
+    AvatarModule,
+    AvatarGroupModule,
+    BadgeModule,
+    BreadcrumbModule,
+    CalendarModule,
+    CardModule,
+    CarouselModule,
+    CascadeSelectModule,
+    CheckboxModule,
+    ChipModule,
+    ChipsModule,
+    ConfirmDialogModule,
+    ConfirmPopupModule,
+    ColorPickerModule,
+    ContextMenuModule,
+    DataViewModule,
+    DialogModule,
+    DividerModule,
+    FieldsetModule,
+    FileUploadModule,
+    TabMenuModule,
+    TabViewModule,
+    TagModule,
+    TerminalModule,
+    TieredMenuModule,
+    TimelineModule,
+    ToggleButtonModule,
+    ToolbarModule,
+    TooltipModule,
+  ],
+})
+export class PrimeNgModule {}
